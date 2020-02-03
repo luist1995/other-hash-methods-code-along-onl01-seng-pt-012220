0e961a11aa01e15b9a75648f8e7f027aff88409f
Luis F Tavarez Valdez <luis.tavarez1995@yahoo.com>
2020-02-03 05:12:30 +0000

    Automatically backed up by Learn

--- lib/values.rb
+++ lib/values.rb
@@ -1,10 +1,12 @@
 require 'pry'
 
-groceries = {
-  dairy: ["milk", "yogurt", "cheese"],
-  vegetable: ["carrots", "broccoli", "cucumbers"],
-  meat: ["chicken", "steak", "salmon"],
-  grains: ["rice", "pasta"]
+#This is the groceries hash we'll be passing in to the get_the_min method
+# groceries = {
+#  dairy: ["milk", "yogurt", "cheese"],
+#  vegetable: ["carrots", "broccoli", "cucumbers"],
+#  meat: ["chicken", "steak", "salmon"],
+#  grains: ["rice", "pasta"]
+# }
 
 def get_the_values(groceries)
   groceries.values.flatten
